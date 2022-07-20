39eb63a66ff66bb82064faf70d2b2b1562890301
Vlad <vlad1matviychuk@gmail.com>
2022-07-20 18:20:25 +0300

    Added slider, video, maps, contact

--- js/script.js
+++ js/script.js
@@ -8,8 +8,28 @@ $(document).ready(function(){
         speed:1000,
         slidesToScroll:1,
         autoplaySpeed:2000,
-        
-
-
     });
+    $('.slider-two').slick({
+        arrows:false,
+        slidesToShow:1,
+        centerMode: true,
+        autoplay:true,
+        speed:1000,
+        slidesToScroll:1,
+        autoplaySpeed:10000,
+        dots: true,
+        waitForAnimate: false,
+        fade: true
+    });
+    $('.slider-three').slick({
+        arrows:false,
+        slidesToShow:1,
+        centerMode: true,
+        autoplay:true,
+        speed:1000,
+        slidesToScroll:1,
+        autoplaySpeed:10000,
+        dots: true,
+        waitForAnimate: false,
+    })
 });
