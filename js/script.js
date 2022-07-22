e2d342c6ba0e2c8dc2d1cd366e682fa7f9566c3b
Vlad <vlad1matviychuk@gmail.com>
2022-07-22 18:21:08 +0300

    Ladt commit

--- js/script.js
+++ js/script.js
@@ -1,13 +1,34 @@
 $(document).ready(function(){
     $('.slider-one').slick({
         arrows:false,
-        adaptiveHeight:true,
         slidesToShow:3,
         centerMode: true,
         autoplay:true,
         speed:1000,
         slidesToScroll:1,
         autoplaySpeed:2000,
+        responsive:[{
+            breakpoint: 851,
+            settings: {
+                slidesToShow:2,
+                centerMode: false,
+                dots: true,
+            }         
+        },{
+            breakpoint: 601,
+            settings: {
+                slidesToShow:1,
+                centerMode: false,
+                dots: true,
+            }
+        },{
+            breakpoint: 301,
+            settings: {
+                slidesToShow:1,
+                centerMode: false,
+                dots: true,
+            }
+        }]
     });
     $('.slider-two').slick({
         arrows:false,
@@ -24,11 +45,14 @@ $(document).ready(function(){
     $('.slider-three').slick({
         arrows:false,
         slidesToShow:1,
-        centerMode: true,
+        
         autoplay:true,
         speed:1000,
         slidesToScroll:1,
         autoplaySpeed:10000,
         dots: true
+    });
+    $('.header-burger').click(function(event){
+        $('.header-burger,.header-list').toggleClass('active');
     })
 });
